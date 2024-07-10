833f47234a3abfe8fb0ad5bc5ffcae86e6e292a2
Rogerio-17 <rogeriojmf10@gmail.com>
2024-07-09 23:48:14 -0300

    feat: adiciona contatos e rota de detalhar produtos

---
+++ src/Icons/ChevronDownIcon.tsx
@@ -0,0 +1,18 @@
+import { Icon, IconProps } from '@chakra-ui/react'
+
+export function ChevronDownIcon(props: IconProps) {
+    return (
+        <Icon
+            width="16px"
+            height="16px"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            {...props}
+        >
+            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+        </Icon>
+    )
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,31 +1,91 @@
-import { Box, Flex, Image, Text } from '@chakra-ui/react'
+import {
+    Box,
+    Button,
+    Divider,
+    Flex,
+    Heading,
+    Image,
+    Link,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
+    Text,
+} from '@chakra-ui/react'
 import { Center } from '../Center'
 import { Search } from '../Search'
 import { ChatIcon } from '../../Icons/ChatIcon'
 import { UserCircleIcon } from '@/Icons/UserCicle'
 import { ShoppingBagIcon } from '@/Icons/ShoppingBag'
 import logo from '@/assets/logo/logo.png'
+import { ChevronDownIcon } from '@/Icons/ChevronDownIcon'
+import { PhoneIcon } from '@/Icons/PhoneIcon'
+import { WhatsAppIcon } from '@/Icons/WhatsAppIcon'
+import { MessageIcon } from '@/Icons/MessageIcon'
+import NextLink from 'next/link'
 
 export function Header() {
     return (
         <Flex as="header" borderBottom="1px solid #c8c6c6">
             <Center justifyContent="space-between" alignItems="center">
                 <Flex alignItems="center" gap="1rem">
-                    <Image src={logo.src} w="120px" h="120px" _ />
+                    <Link as={NextLink} href="/">
+                        <Image src={logo.src} w="120px" h="120px" _ />
+                    </Link>
+
                     <Search w="30rem" />
                 </Flex>
 
-                <Flex gap="0.5rem">
-                    <Flex w="9rem" fontSize="14px" lineHeight="16px" alignItems="center" gap="5px">
+                <Flex gap="1rem">
+                    <Flex alignItems="center" gap="0.5rem">
                         <ChatIcon w="30px" h="30px" color="green" />{' '}
-                        <Text>
-                            Central de <strong>atendimento</strong>
-                        </Text>
+                        <Menu closeOnSelect={false}>
+                            <MenuButton
+                                textAlign="left"
+                                w="7.5rem"
+                                as={Button}
+                                rightIcon={<ChevronDownIcon />}
+                                bg="transparent"
+                                p="10px 0px"
+                                _active="none"
+                                _hover="none"
+                            >
+                                Central de <Text fontWeight="bold">atendimento</Text>
+                            </MenuButton>
+                            <MenuList>
+                                <Text pl="12px" pb="6px" fontSize="1.1rem" fontWeight="bold">
+                                    Contatos
+                                </Text>
+                                <Divider />
+                                <Flex mt="6px" flexDirection="column" p="0px 12px" gap="8px">
+                                    <Flex alignItems="center" gap="0.5rem">
+                                        <PhoneIcon />
+                                        <Text>(84) 98810-3345</Text>
+                                    </Flex>
+
+                                    <Flex alignItems="center" gap="0.5rem">
+                                        <WhatsAppIcon />
+                                        <Text>(84) 98810-3345</Text>
+                                    </Flex>
+
+                                    <Flex alignItems="center" gap="0.5rem">
+                                        <MessageIcon w="18px" h="18px" />
+                                        <Text>belezanordestinarn@gmail.com</Text>
+                                    </Flex>
+                                </Flex>
+                            </MenuList>
+                        </Menu>
                     </Flex>
-                    <Flex w="9rem" fontSize="14px" lineHeight="16px" alignItems="center" gap="5px">
+                    <Flex
+                        w="9.3rem"
+                        fontSize="14px"
+                        lineHeight="16px"
+                        alignItems="center"
+                        gap="5px"
+                    >
                         <UserCircleIcon w="30px" h="30px" color="green" />{' '}
                         <Text>
-                            Seja bem-vindo <strong>Visitante</strong>
+                            Seja bem-vindo(a) <strong>Visitante</strong>
                         </Text>
                     </Flex>
                     <Flex>
